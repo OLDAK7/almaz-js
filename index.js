984f72889752a7ee76093e33e077dbf7ef0b5630
OLDAK7 <almaz.karimovriddle@mail.ru>
2021-12-28 23:47:52 +0500

    upd index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
- console.log("Hello, Almaz!");
+console.log("Hello, Almaz!");
+
+function reverse(s) {
+    return s && s[s.length - 1] + reverse(s.slice(0, -1))
+}
+
+console.log(reverse("motherFucker"));
