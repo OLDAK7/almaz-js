225d6e21641133136d0af2106dbb3257cb584212
OLDAK7 <almaz.karimovriddle@mail.ru>
2022-01-25 15:50:57 +0500

    add tests files

---
+++ capitalize.js
@@ -0,0 +1,8 @@
+const capitalize = (text) => {
+    const firstChar = text[0].toUpperCase();
+    const restSubstring = text.slice(1);
+    return `${firstChar}${restSubstring}`;
+};
+
+console.log(capitalize('hello'));
+
