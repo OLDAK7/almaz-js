bb7c4ff3ed27511fc3a658a94cc15717c71d2784
OLDAK7 <almaz.karimovriddle@mail.ru>
2022-05-25 13:39:19 +0500

    add indexHtml

--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,1 @@
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+console.log('Тестим git!');
